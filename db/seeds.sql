ccf666bce3b34e2504414e0b42ecb0d0c3f7fec8
JAngel <43564263+jenaym@users.noreply.github.com>
2019-01-29 18:52:02 -0800

    sql connected

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Impossible Burger");
+INSERT INTO burgers (burger_name) VALUES ("Gigantic Burger");
+INSERT INTO burgers (burger_name) VALUES ("All You Can EAT Burger");
+
